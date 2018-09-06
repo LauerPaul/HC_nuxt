4359513977d68da7eae072cd6626f7ba7ad30edb
Paul <pavel.ugrynovych@lauer.com.ua>
2018-09-07 00:01:57 +0300

    update structure 2

--- .tasks/git.js
+++ .tasks/git.js
@@ -2,44 +2,12 @@ import gulp from 'gulp'
 import git from 'gulp-git'
 import log from 'gutil-color-log'
 
-export function git_status(cb){
+export function git(cb){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [status] ');
     console.log('-----------------------------------------------------------------------------------');
-
-	git.status(function (err, stdout) {
-		if (err) throw err;
-	});
-    
+	git.status(function (err, stdout) { if (err) throw err;	});
+    git.push('origin', 'master', function (err) { if (err) throw err; });
     if(typeof(cb) == 'function') cb();
-}
-
-export function git_add(cb){
-	return gulp.src('./')
-		.pipe(git.add());
-}
-
-export function git_commit(cb){
-	let newVersion;
-	function computeNewVersion() { 
-		Date.prototype.yyyymmdd = function() {
-			var mm = this.getMonth() + 1;
-			var dd = this.getDate();
-
-			return [this.getFullYear()+'-',
-			      (mm>9 ? '' : '0') + mm+'-',
-			      (dd>9 ? '' : '0') + dd
-			     ].join('');
-		};
-
-		var date = new Date();
-		var time = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
-		newVersion = date.yyyymmdd();
-		newVersion += ' (' + time + ')';
-	}
-	
-	return gulp.src('./')
-			.pipe(computeNewVersion())
-			.pipe(git.commit(() => `gulp-git commit [${newVersion}]`));
 }
--- .tasks/index.js
+++ .tasks/index.js
@@ -3,12 +3,10 @@ import { styles } from './styles'
 import { img }  from './images'
 import { watch }  from './watch'
 import { img_del }  from './del'
-import { git_status }  from './git'
-import { git_add }  from './git'
-import { git_commit }  from './git'
+import { git }  from './git'
 
 export const dev   = gulp.series( gulp.parallel(styles, img), watch )
 export const build = gulp.series( gulp.parallel(styles, img) )
-export const deploy = gulp.series( gulp.parallel(git_status, git_add, git_status) )
+export const deploy = gulp.series( gulp.parallel(git) )
 
 export default dev
