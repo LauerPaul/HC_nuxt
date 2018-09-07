4413cb1652962e1287313809bca383540ddd48f9
Paul <pavel.ugrynovych@lauer.com.ua>
2018-09-07 14:03:28 +0300

    initial commit

--- .tasks/git.js
+++ .tasks/git.js
@@ -2,41 +2,44 @@ import gulp from 'gulp'
 import git from 'gulp-git'
 import log from 'gutil-color-log'
 
-gulp.task('git_push', function(){
+gulp.task('git_push', function(cb){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git push]');
     console.log('-----------------------------------------------------------------------------------');
 
-	return git.push('origin', 'master', function (err) { if (err) throw err; });
+	git.push('origin', 'master', function (err) { if (err) throw err; });
+	cb();
 });
 
-gulp.task('git_commit', function(){
+gulp.task('git_commit', function(cb){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git commit]');
     console.log('-----------------------------------------------------------------------------------');
 
-	return gulp.src('./')
-	    		.pipe(git.commit('initial commit'));
+	gulp.src('./').pipe(git.commit('initial commit'));
+	cb();
 });
 
-gulp.task('git_add', function(){
+gulp.task('git_add', function(cb){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git add]');
     console.log('-----------------------------------------------------------------------------------');
 
-	return gulp.src('./').pipe(git.add())
+	gulp.src('./').pipe(git.add())
+	cb();
 });
 
-gulp.task('git_status', function(){
+gulp.task('git_status', function(cb){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git status]');
     console.log('-----------------------------------------------------------------------------------');
 
-	return	git.status({args: '--porcelain'}, function (err, stdout) { if (err) throw err; });
+	git.status({args: '--porcelain'}, function (err, stdout) { if (err) throw err; });
+	cb();
 });
 
 gulp.task('deploy', gulp.series('git_status', 'git_add', 'git_commit', 'git_status', 'git_push'));
