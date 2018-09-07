685292995b79c52eb7bbef80bf87eb55cb8b394b
Paul <pavel.ugrynovych@lauer.com.ua>
2018-09-07 15:11:36 +0300

    gulp-git commit [2018-9-7 15:11:36]

--- .tasks/index.js
+++ .tasks/index.js
@@ -3,9 +3,9 @@ import { styles } from './styles'
 import { img }  from './images'
 import { watch }  from './watch'
 import { img_del }  from './del'
-import './git'
 
 export const dev   = gulp.series( gulp.parallel(styles, img), watch )
 export const build = gulp.series( gulp.parallel(styles, img) )
 
 export default dev
+
---
+++ .tasks/ssh.js
@@ -0,0 +1,24 @@
+const
+fs = require('fs'),
+gulp = require('gulp'),
+GulpSSH = require('gulp-ssh')
+ 
+const config = {
+  host: '91.211.248.24',
+  port: 22,
+  username: 'sexmeet',
+  password: 'sexmeet',
+  // privateKey: fs.readFileSync('/Users/zensh/.ssh/id_rsa')
+},
+
+gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: config
+})
+ 
+gulp.task('status', function (cb){
+  gulpSSH.shell(['cd /home/sexmeet/www/nuxt.hearts-club.com/public_html', 'git status'], {filePath: 'shell.log'})
+  .pipe(gulp.dest('../.log'))
+
+  cb();
+})
