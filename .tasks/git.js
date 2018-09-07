a6e586208d283a2d10bd28bbce3fd59e9bfcf5c3
Paul <pavel.ugrynovych@lauer.com.ua>
2018-09-07 13:58:12 +0300

    initial commit

--- .tasks/git.js
+++ .tasks/git.js
@@ -2,12 +2,48 @@ import gulp from 'gulp'
 import git from 'gulp-git'
 import log from 'gutil-color-log'
 
-export function git(cb){
+gulp.task('git_commit', function(){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
-    log('green', '->>>>>>> ✅ GIT [status] ');
+    log('green', '->>>>>>> ✅ GIT [git commit]');
     console.log('-----------------------------------------------------------------------------------');
-	git.status(function (err, stdout) { if (err) throw err;	});
-    git.push('origin', 'master', function (err) { if (err) throw err; });
-    if(typeof(cb) == 'function') cb();
-}
+
+	return gulp.src('./')
+	    		.pipe(git.commit('initial commit'));
+
+});
+
+gulp.task('git_add', function(cb){
+	console.log(' ');
+    console.log('-----------------------------------------------------------------------------------');
+    log('green', '->>>>>>> ✅ GIT [git add]');
+    console.log('-----------------------------------------------------------------------------------');
+
+	gulp.src('./').pipe(git.add())
+
+	cb();
+});
+
+gulp.task('git_status', function(cb){
+	console.log(' ');
+    console.log('-----------------------------------------------------------------------------------');
+    log('green', '->>>>>>> ✅ GIT [git status]');
+    console.log('-----------------------------------------------------------------------------------');
+
+	git.status({args: '--porcelain'}, function (err, stdout) { if (err) throw err; });
+
+	cb();
+});
+
+gulp.task('deploy', gulp.series('git_status', 'git_add', 'git_status', 'git_commit', 'git_status'));
+
+
+
+	// })
+	// // if(git.status(function (err, stdout) { if (err) throw err;	})){
+	// // 	gulp.src('./').pipe(git.add())
+	// // 	git.status(function (err, stdout) { if (err) throw err;	})
+	// // }
+	// 	// git.status(function (err, stdout) { if (err) throw err;	})
+ //    	// .push('origin', 'master', function (err) { if (err) throw err; });
+	// cb();
--- .tasks/index.js
+++ .tasks/index.js
@@ -3,10 +3,9 @@ import { styles } from './styles'
 import { img }  from './images'
 import { watch }  from './watch'
 import { img_del }  from './del'
-import { git }  from './git'
+import './git'
 
 export const dev   = gulp.series( gulp.parallel(styles, img), watch )
 export const build = gulp.series( gulp.parallel(styles, img) )
-export const deploy = gulp.series( gulp.parallel(git) )
 
 export default dev
