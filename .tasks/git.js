4499a78eb4fd115ea25a586179bb83e6e415d9f8
Paul <pavel.ugrynovych@lauer.com.ua>
2018-09-07 14:02:13 +0300

    initial commit

--- .tasks/git.js
+++ .tasks/git.js
@@ -2,6 +2,15 @@ import gulp from 'gulp'
 import git from 'gulp-git'
 import log from 'gutil-color-log'
 
+gulp.task('git_push', function(){
+	console.log(' ');
+    console.log('-----------------------------------------------------------------------------------');
+    log('green', '->>>>>>> ✅ GIT [git push]');
+    console.log('-----------------------------------------------------------------------------------');
+
+	return git.push('origin', 'master', function (err) { if (err) throw err; });
+});
+
 gulp.task('git_commit', function(){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
@@ -10,32 +19,27 @@ gulp.task('git_commit', function(){
 
 	return gulp.src('./')
 	    		.pipe(git.commit('initial commit'));
-
 });
 
-gulp.task('git_add', function(cb){
+gulp.task('git_add', function(){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git add]');
     console.log('-----------------------------------------------------------------------------------');
 
-	gulp.src('./').pipe(git.add())
-
-	cb();
+	return gulp.src('./').pipe(git.add())
 });
 
-gulp.task('git_status', function(cb){
+gulp.task('git_status', function(){
 	console.log(' ');
     console.log('-----------------------------------------------------------------------------------');
     log('green', '->>>>>>> ✅ GIT [git status]');
     console.log('-----------------------------------------------------------------------------------');
 
-	git.status({args: '--porcelain'}, function (err, stdout) { if (err) throw err; });
-
-	cb();
+	return	git.status({args: '--porcelain'}, function (err, stdout) { if (err) throw err; });
 });
 
-gulp.task('deploy', gulp.series('git_status', 'git_add', 'git_status', 'git_commit', 'git_status'));
+gulp.task('deploy', gulp.series('git_status', 'git_add', 'git_commit', 'git_status', 'git_push'));
 
 
 
